508d67ea7608c9d65f465d96140b7d0c60289f5b
Auras Paltanea <92265800+AURAS-ux@users.noreply.github.com>
2023-03-28 09:59:50 +0300

    Initial commit

---
+++ proiect/src/app/pages/inventory/inventory.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory',
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./inventory.component.scss']
+})
+export class InventoryComponent {
+  items:string[] = [
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea",
+    "Vanea"
+  ];
+}
